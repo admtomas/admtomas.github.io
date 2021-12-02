47c80713ff0bda0ec6d24690bbe88f72e9d240ab
Tomasz <admtomas@gmail.com>
2021-12-02 19:57:18 +0000

    Work experience filled up

--- assets/js/hide.js
+++ assets/js/hide.js
@@ -1,15 +1,39 @@
 // Hide show part of the txt for work experience
 $(document).ready(function(){
-    $("#show").click(function(){
-        $("#toggled").toggle();
+    $("#show-one").click(function(){
+        $("#toggled-one").toggle();
         $(this).hide();
-        $("#hide").show();
+        $("#hide-one").show();
     });
 
-    $("#hide").click(function(){
-        $("#toggled").toggle();
+    $("#hide-one").click(function(){
+        $("#toggled-one").toggle();
         $(this).hide();
-        $('#show').show();
+        $('#show-one').show();
+    });
+
+    $("#show-two").click(function(){
+        $("#toggled-two").toggle();
+        $(this).hide();
+        $("#hide-two").show();
+    });
+
+    $("#hide-two").click(function(){
+        $("#toggled-two").toggle();
+        $(this).hide();
+        $('#show-two').show();
+    });
+
+    $("#show-three").click(function(){
+        $("#toggled-three").toggle();
+        $(this).hide();
+        $("#hide-three").show();
+    });
+
+    $("#hide-three").click(function(){
+        $("#toggled-three").toggle();
+        $(this).hide();
+        $('#show-three').show();
     });
 
 });
